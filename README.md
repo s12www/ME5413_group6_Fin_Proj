25ecc2707f77fc53c4e9749f5fb9828dff9a1239
s12www <2412144700@qq.com>
2024-04-09 23:07:04 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,146 @@
+# Autonomous Mobile Robot Mapping & Navigation in Virtual Factory
+
+![Ubuntu 20.04](https://img.shields.io/badge/OS-Ubuntu_20.04-informational?style=flat&logo=ubuntu&logoColor=white&color=2bbc8a)
+![ROS Noetic](https://img.shields.io/badge/Tools-ROS_Noetic-informational?style=flat&logo=ROS&logoColor=white&color=2bbc8a)
+![C++](https://img.shields.io/badge/Code-C++-informational?style=flat&logo=c%2B%2B&logoColor=white&color=2bbc8a)
+![Python](https://img.shields.io/badge/Code-Python-informational?style=flat&logo=Python&logoColor=white&color=2bbc8a)
+
+This is the final project report for the **ME5413 Autonomous Mobile Robotics course at the National University of Singapore**, completed by Group 6.
+
+**Authors:**   chen Guanyu, Wu Zhuojun, Xu Boyang, Hao Yuqi, Miao Chenhui, Zhou Yinhong
+
+
+<h2 id="1"> Installation</h2>
+
+This repo is a ros workspace, containing three rospkgs:
+* `interactive_tools` are customized tools to interact with gazebo and your robot
+* `jackal_description` contains the modified jackal robot model descriptions
+* `me5413_world` the main pkg containing the gazebo world, and the launch files
+
+**Note:** If you are working on this project, it is encouraged to fork this repository and work on your own fork!
+
+After forking this repo to your own github:
+
+```bash
+# Clone your own fork of this repo (assuming home here `~/`)
+cd
+git clone https://github.com/<YOUR_GITHUB_USERNAME>/ME5413_Final_Project.git
+cd ME5413_Final_Project
+
+# Install all dependencies
+rosdep install --from-paths src --ignore-src -r -y
+
+# Build
+catkin_make
+# Source
+source devel/setup.bash
+```
+
+To properly load the gazebo world, you will need to have the necessary model files in the `~/.gazebo/models/` directory.
+
+<h2 id="1"> 1. Project Decription</h2>
+
+In this mini-factory environment:
+* 3 target areas
+* 1 restricted area
+
+The aim of the project is to design a robot navigation software stack that can:
+* From the starting point, move to the given pose within each area in sequence:
+  * Assembly Line 1, 2
+  * Random Box 1, 2, 3, 4
+  * Delivery Vehicle 1, 2, 3
+
+Task 1 Mapping
+
+In this task, we manually map the environment using SLAM.
+* Maps Building: use the SLAM alogrithms to map the environment
+  * SLAM Point LIO: for its accuracy 
+  * SLAM ALOAM & FLOAM: for the modernity 
+* Maps Generating: caputure 3D point cloud maps through SLAM and convert them to 2D maps
+  * 3D point cloud maps saving
+  * Filter the useless points and adjust the orientations of PCDs
+  * Convert 3D maps to 2D maps
+ 
+Task 2 Navigation
+
+First, we preprocess the map using a costmap, which involves inflating the boundaries and setting obstacles on impassable paths. Subsequently, we employ AMCL localization method to localize the vehicle. Following localization, we perform path planning for the vehicle. For global planning, we utilize a hybrid A* planner that combines navfn and global_planner. As for local planning, we employ DWA and TED strategies.
+
+By integrating these various methods, we aim to achieve more robust and efficient path planning results. We will compare and analyze these different planning approaches to better understand their pros and cons, providing insights for practical applications.
+  
+
+<h2 id="2"> 2. Prerequisites</h2>
+
+### 2.1 **Ubuntu** and **ROS**
+* Ubuntu = 20.04
+* ROS    = Noetic. [ROS Installation](http://wiki.ros.org/ROS/Installation)
+
+### 2.2 **PCL && Eigen**
+* PCL    >= 1.8,   Follow [PCL Installation](http://www.pointclouds.org/downloads/linux.html).
+* Eigen  >= 3.3.4, Follow [Eigen Installation](http://eigen.tuxfamily.org/index.php?title=Main_Page).
+
+### 2.3 **livox_ros_driver**
+* Follow [livox_ros_driver Installation](https://github.com/Livox-SDK/livox_ros_driver).
+
+
+
+### Task 1 Mapping
+After building the whole files, in the folder starting Point_LIO:
+```bash
+# Launch Gazebo World together with our robot
+roslaunch me5413_world world.launch
+
+# In another terminal
+roslaunch me5413_world Point_lio.launch
+```
+Once you mapped the whole world you can find the **PCD** files in ~/src/Point_LIO/PCD.
+
+If you want to use ALOAM to get a 3D map:
+```bash
+# Launch Gazebo World together with our robot
+source devel/setup.bash
+roslaunch me5413_world world.launch
+
+# In another terminal
+source devel/setup.bash
+roslaunch me5413_world aloam.launch
+
+# Get the rosbags of the map (you can start a new terminal to do so)
+# This performance can be done after you maped the whole world but the terminal does not be closed
+rosbag record /laser_cloud_map -O map3
+rosrun pcl_ros bag_to_pcd map3.bag /laser_cloud_map map3.pcd
+```
+
+You may process the useless point in the 3D PCD file and convert the map to a 2D map:
+```bash
+# Use pass through filer and Radius Outlier Removal
+# Put the PCD maps in the home directory
+source devel/setup.bash
+roslaunch pcd2pgm run.launch
+
+rosrun map_server map_saver
+
+#If the pcd map's orientations are not good, you can rotate it first (This script also has the function to filter the point)
+# Put the PCD maps in the home directory
+source devel/setup.bash
+roslaunch read_pcd read_pcd.launch 
+```
+If you want to use EVO to evaluate the performance:
+```bash
+# Record the datas in the beginning in a new terminal
+rosbag record /gazebo/ground_truth/state /odometry/filtered -O Truth_Odom.bag
+
+# After that:
+evo_ape bag Truth_Odom.bag /gazebo/ground_truth/state /odometry/filtered -r full -va --plot --plot_mode xy
+```
+### Task 2 Navigation
+
+After completing the mapping, end the mapping process and restart gazebo. To load the navigation stack, execute the following command in a new terminal window.
+```
+source devel/setup.bash
+roslaunch me5413_world world.launch
+
+source devel/setup.bash
+roslaunch me5413_world navigation.launch
+```
+Select the relevant topic under global path, and then press the button on simplePanel to set the desired goal pose.
+
---
+++ src/me5413_world/src/goal_publisher_node.cpp
@@ -0,0 +1,336 @@
+/* goal_publisher_node.cpp
+
+ * Copyright (C) 2023 SS47816
+
+ * ROS Node for publishing goal poses 
+ 
+**/
+
+#include "me5413_world/goal_publisher_node.hpp"
+
+namespace me5413_world 
+{
+
+GoalPublisherNode::GoalPublisherNode() : tf2_listener_(tf2_buffer_)
+{
+  this->pub_goal_ = nh_.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 1);
+  this->pub_absolute_position_error_ = nh_.advertise<std_msgs::Float32>("/me5413_world/absolute/position_error", 1);
+  this->pub_absolute_heading_error_ = nh_.advertise<std_msgs::Float32>("/me5413_world/absolute/heading_error", 1);
+  this->pub_relative_position_error_ = nh_.advertise<std_msgs::Float32>("/me5413_world/relative/position_error", 1);
+  this->pub_relative_heading_error_ = nh_.advertise<std_msgs::Float32>("/me5413_world/relative/heading_error", 1);
+
+  this->timer_ = nh_.createTimer(ros::Duration(0.2), &GoalPublisherNode::timerCallback, this);
+  this->sub_robot_odom_ = nh_.subscribe("/gazebo/ground_truth/state", 1, &GoalPublisherNode::robotOdomCallback, this);
+  this->sub_goal_name_ = nh_.subscribe("/rviz_panel/goal_name", 1, &GoalPublisherNode::goalNameCallback, this);
+  this->sub_goal_pose_ = nh_.subscribe("/move_base_simple/goal", 1, &GoalPublisherNode::goalPoseCallback, this);
+  this->sub_box_markers_ = nh_.subscribe("/gazebo/ground_truth/box_markers", 1, &GoalPublisherNode::boxMarkersCallback, this);
+  
+  // Initialization
+  this->robot_frame_ = "base_link";
+  this->map_frame_ = "map";
+  this->world_frame_ = "world";
+  this->absolute_position_error_.data = 0.0;
+  this->absolute_heading_error_.data = 0.0;
+  this->relative_position_error_.data = 0.0;
+  this->relative_heading_error_.data = 0.0;
+  this->last_responded_goal_id_ = "";
+  this->last_goal_time_ = ros::Time::now();
+};
+
+void GoalPublisherNode::timerCallback(const ros::TimerEvent&)
+{
+  // Calculate absolute errors (wrt to world frame)
+  const std::pair<double, double> error_absolute = calculatePoseError(this->pose_world_robot_, this->pose_world_goal_);
+  // Calculate relative errors (wrt to map frame)
+  const std::pair<double, double> error_relative = calculatePoseError(this->pose_map_robot_, this->pose_map_goal_);
+  
+  this->absolute_position_error_.data = error_absolute.first;
+  this->absolute_heading_error_.data = error_absolute.second;
+  this->relative_position_error_.data = error_relative.first;
+  this->relative_heading_error_.data = error_relative.second;
+
+  if (this->goal_type_ == "box")
+  {
+    this->absolute_heading_error_.data = 0.0;
+    this->relative_heading_error_.data = 0.0;
+  }
+
+  // Publish errors
+  this->pub_absolute_position_error_.publish(this->absolute_position_error_);
+  this->pub_absolute_heading_error_.publish(this->absolute_heading_error_);
+  this->pub_relative_position_error_.publish(this->relative_position_error_);
+  this->pub_relative_heading_error_.publish(this->relative_heading_error_);
+
+  return;
+};
+
+void GoalPublisherNode::robotOdomCallback(const nav_msgs::Odometry::ConstPtr& odom)
+{
+  this->world_frame_ = odom->header.frame_id;
+  this->robot_frame_ = odom->child_frame_id;
+  this->pose_world_robot_ = odom->pose.pose;
+
+  const tf2::Transform T_world_robot = convertPoseToTransform(this->pose_world_robot_);
+  const tf2::Transform T_robot_world = T_world_robot.inverse();
+
+  geometry_msgs::TransformStamped transformStamped;
+  transformStamped.header.stamp = ros::Time::now();
+  transformStamped.header.frame_id = this->robot_frame_;
+  transformStamped.child_frame_id = this->world_frame_;
+  transformStamped.transform.translation.x = T_robot_world.getOrigin().getX();
+  transformStamped.transform.translation.y = T_robot_world.getOrigin().getY();
+  transformStamped.transform.translation.z = 0.0;
+  transformStamped.transform.rotation.x = T_robot_world.getRotation().getX();
+  transformStamped.transform.rotation.y = T_robot_world.getRotation().getY();
+  transformStamped.transform.rotation.z = T_robot_world.getRotation().getZ();
+  transformStamped.transform.rotation.w = T_robot_world.getRotation().getW();
+  
+  this->tf2_bcaster_.sendTransform(transformStamped);
+
+  return;
+};
+
+void GoalPublisherNode::goalNameCallback(const std_msgs::String::ConstPtr& name)
+{
+  const std::string goal_name = name->data;
+  const int end = goal_name.find_last_of("_");
+  this->goal_type_ = goal_name.substr(1, end-1);
+  const int goal_box_id = stoi(goal_name.substr(end+1, 1));
+
+  geometry_msgs::PoseStamped P_world_goal;
+  if (this->goal_type_ == "box")
+  {
+    if (box_poses_.empty())
+    {
+      ROS_ERROR_STREAM("Box poses unknown, please spawn boxes first!");
+      return;
+    }
+    else if (goal_box_id >= box_poses_.size())
+    {
+      ROS_ERROR_STREAM("Box id is outside the available range, please select a smaller id!");
+      return;
+    }
+    
+    P_world_goal = box_poses_[goal_box_id - 1];
+  }
+  else
+  {
+    // Get the Pose of the goal in world frame
+    P_world_goal = getGoalPoseFromConfig(goal_name);
+  }
+
+  this->pose_world_goal_ = P_world_goal.pose;
+  // Get the Transform from world to map from the tf_listener
+  geometry_msgs::TransformStamped transform_map_world;
+  try
+  {
+    transform_map_world = this->tf2_buffer_.lookupTransform(this->map_frame_, this->world_frame_, ros::Time(0));
+  }
+  catch (tf2::TransformException& ex)
+  {
+    ROS_WARN("%s", ex.what());
+    return;
+  }
+
+  // Transform the goal pose to map frame
+  geometry_msgs::PoseStamped P_map_goal;
+  tf2::doTransform(P_world_goal, P_map_goal, transform_map_world);
+  P_map_goal.header.stamp = ros::Time::now();
+  P_map_goal.header.frame_id = map_frame_;
+
+  // Transform the robot pose to map frame
+  tf2::doTransform(this->pose_world_robot_, this->pose_map_robot_, transform_map_world);
+
+  this->pub_goal_.publish(P_map_goal);
+  /*// Publish goal pose in map frame 
+  if (this->goal_type_ != "box")
+  {
+    this->pub_goal_.publish(P_map_goal);
+  }
+  else
+  {
+    // 如果目标类型为盒子，则将机器人导航到固定位置后执行旋转动作
+    // 导航到指定位置（12.5，1.2）
+    P_map_goal.pose.position.x = 12.5;
+    P_map_goal.pose.position.y = 1.2;
+    this->pub_goal_.publish(P_map_goal);
+  }*/
+  return;
+};
+
+void GoalPublisherNode::moveBaseStatusCallback(const actionlib_msgs::GoalStatusArray& status)
+{
+  // 定义五个固定点的坐标
+  std::vector<geometry_msgs::PoseStamped> fixedPoints;
+  
+  // 添加五个固定点的坐标
+  geometry_msgs::PoseStamped point1, point2, point3, point4, point5;
+  point1.pose.position.x = 12.5;
+  point1.pose.position.y = 1.2;
+  point1.pose.orientation.w = 1.0;
+  point1.header.frame_id = map_frame_;
+
+  point2.pose.position.x = 12.5;
+  point2.pose.position.y = 2.0;
+  point2.pose.orientation.w = 1.0;
+  point2.header.frame_id = map_frame_;
+
+  point3.pose.position.x = 12.5;
+  point3.pose.position.y = 3.0;
+  point3.pose.orientation.w = 1.0;
+  point3.header.frame_id = map_frame_;
+
+  point4.pose.position.x = 12.5;
+  point4.pose.position.y = 4.0;
+  point4.pose.orientation.w = 1.0;
+  point4.header.frame_id = map_frame_;
+
+  point5.pose.position.x = 12.5;
+  point5.pose.position.y = 5.0;
+  point5.pose.orientation.w = 1.0;
+  point5.header.frame_id = map_frame_;
+
+  // 将五个固定点加入数组
+  fixedPoints.push_back(point1);
+  fixedPoints.push_back(point2);
+  fixedPoints.push_back(point3);
+  fixedPoints.push_back(point4);
+  fixedPoints.push_back(point5);
+
+  // 确保固定点的个数大于0
+  if (fixedPoints.size() == 0)
+  {
+    ROS_WARN("No fixed points defined.");
+    return;
+  }
+
+  // 获取当前时间与上一个目标的响应时间间隔
+  double time_diff = (ros::Time::now() - last_goal_time_).toSec();
+
+  // 遍历执行目标的状态数组
+  for (const auto& goalStatus : status.status_list)
+  {
+    // 检查当前目标的ID是否与上一个已响应的目标ID不同，且响应时间间隔是否大于0.3秒
+    if (goalStatus.goal_id.id != last_responded_goal_id_ && time_diff > 0.3)
+    {
+      last_goal_time_ = ros::Time::now();
+
+      // 检查当前目标的状态是否为已成功或者已中止
+      if (goalStatus.status == actionlib_msgs::GoalStatus::SUCCEEDED ||
+          goalStatus.status == actionlib_msgs::GoalStatus::ABORTED)
+      {
+        // 检查目标类型是否为"box"
+        if (this->goal_type_ == "box")
+        {
+          // 将当前目标的ID标记为上一个已响应的目标ID
+          this->last_responded_goal_id_ = goalStatus.goal_id.id;
+
+          // 计算下一个固定点的索引
+          static int nextPointIndex = 0;
+          nextPointIndex = (nextPointIndex + 1) % fixedPoints.size();
+
+          // 获取下一个固定点的坐标
+          geometry_msgs::PoseStamped nextGoal = fixedPoints[nextPointIndex];
+
+          // 更新目标的时间戳为当前时间
+          nextGoal.header.stamp = ros::Time::now();
+
+          // 更新目标的坐标系为地图坐标系
+          nextGoal.header.frame_id = map_frame_;
+
+          // 发布下一个固定点的目标
+          this->pub_goal_.publish(nextGoal);
+
+          break;
+        }
+      }
+    }
+  }
+}
+
+void GoalPublisherNode::goalPoseCallback(const geometry_msgs::PoseStamped::ConstPtr& goal_pose)
+{
+  this->pose_map_goal_ = goal_pose->pose;
+}
+
+tf2::Transform GoalPublisherNode::convertPoseToTransform(const geometry_msgs::Pose& pose)
+{
+  tf2::Transform T;
+  T.setOrigin(tf2::Vector3(pose.position.x, pose.position.y, 0));
+  tf2::Quaternion q;
+  q.setValue(pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w);
+  T.setRotation(q);
+
+  return T;
+};
+
+void GoalPublisherNode::boxMarkersCallback(const visualization_msgs::MarkerArray::ConstPtr& box_markers)
+{
+  this->box_poses_.clear();
+  for (const auto& box : box_markers->markers)
+  {
+    geometry_msgs::PoseStamped pose;
+    pose.pose = box.pose;
+    this->box_poses_.emplace_back(pose);
+  }
+
+  return;
+};
+
+geometry_msgs::PoseStamped GoalPublisherNode::getGoalPoseFromConfig(const std::string& name)
+{
+  /** 
+   * Get the Transform from goal to world from the file
+   */
+
+  double x, y, yaw;
+  nh_.getParam("/me5413_world" + name + "/x", x);
+  nh_.getParam("/me5413_world" + name + "/y", y);
+  nh_.getParam("/me5413_world" + name + "/yaw", yaw);
+  nh_.getParam("/me5413_world/frame_id", this->world_frame_);
+
+  tf2::Quaternion q;
+  q.setRPY(0, 0, yaw);
+  q.normalize();
+
+  geometry_msgs::PoseStamped P_world_goal;
+  P_world_goal.pose.position.x = x;
+  P_world_goal.pose.position.y = y;
+  P_world_goal.pose.orientation = tf2::toMsg(q);
+
+  return P_world_goal;
+};
+
+std::pair<double, double> GoalPublisherNode::calculatePoseError(const geometry_msgs::Pose& pose_robot, const geometry_msgs::Pose& pose_goal)
+{
+  // Positional Error
+  const double position_error = std::sqrt(
+    std::pow(pose_robot.position.x - pose_goal.position.x, 2) + 
+    std::pow(pose_robot.position.y - pose_goal.position.y, 2)
+  );
+
+  // Heading Error
+  tf2::Quaternion q_robot, q_goal;
+  tf2::fromMsg(pose_robot.orientation, q_robot);
+  tf2::fromMsg(pose_goal.orientation, q_goal);
+  const tf2::Matrix3x3 m_robot = tf2::Matrix3x3(q_robot);
+  const tf2::Matrix3x3 m_goal = tf2::Matrix3x3(q_goal);
+
+  double roll, pitch, yaw_robot, yaw_goal;
+  m_robot.getRPY(roll, pitch, yaw_robot);
+  m_goal.getRPY(roll, pitch, yaw_goal);
+
+  const double heading_error = (yaw_robot - yaw_goal)/M_PI*180.0;
+
+  return std::pair<double, double>(position_error, heading_error);
+}
+
+} // namespace me5413_world
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "goal_publisher_node");
+  me5413_world::GoalPublisherNode goal_publisher_node;
+  ros::spin();  // spin the ros node.
+  return 0;
+}
